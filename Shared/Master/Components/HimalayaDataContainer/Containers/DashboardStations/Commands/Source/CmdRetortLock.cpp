dd8ad2e3166919e90efdf3732a31950be87e3a78
AbeYang <AbeYang@3e712691-2e8f-cf49-9ccc-6e7238dc2b4e>
2013-03-25 07:20:42 +0000

    Missed two file in my last submission.
    git-svn-id: https://lmslmc-dc02.ap.leicams.com/svn/softwareTeam/Himalaya/branches/B0010_GUI/Himalaya@444 3e712691-2e8f-cf49-9ccc-6e7238dc2b4e

---
+++ Shared/Master/Components/HimalayaDataContainer/Containers/DashboardStations/Commands/Source/CmdRetortLock.cpp
@@ -0,0 +1,59 @@
+/****************************************************************************/
+/*! \file CmdRetortLock.cpp
+ *
+ *  \brief CmdRetortLock command implementation.
+ *
+ *   $Version: $ 0.1
+*   $Date:    $ 25.03.2013
+ *   $Author:  $ Abe Yang
+ *
+ *  \b Company:
+ *
+  *       Leica Microsystems Ltd. Shanghai.
+ *
+ *  (C) Copyright 2012 by Leica Microsystems Shanghai. All rights reserved.
+ *  This is unpublished proprietary source code of Leica. The copyright notice
+ *  does not evidence any actual or intended publication.
+ *
+ */
+/****************************************************************************/
+
+#include "HimalayaDataContainer/Containers/DashboardStations/Commands/Include/CmdRetortLock.h"
+namespace MsgClasses {
+
+QString CmdRetortLock::NAME = "MsgClasses::CmdRetortLock";
+
+
+CmdRetortLock::CmdRetortLock(int Timeout, bool IsLock) :
+    Command(Timeout),
+    m_Lock(IsLock)
+{
+}
+
+CmdRetortLock::CmdRetortLock()
+    : Command(0)
+{
+}
+
+/****************************************************************************/
+/*!
+ *  \brief   Destructor
+ */
+/****************************************************************************/
+CmdRetortLock::~CmdRetortLock()
+{
+}
+
+/****************************************************************************/
+/*!
+ *  \brief   Get command name
+ *
+ *  \return  command name as string
+ */
+/****************************************************************************/
+QString CmdRetortLock::GetName() const
+{
+    return NAME;
+}
+
+} // end namespace MsgClasses
