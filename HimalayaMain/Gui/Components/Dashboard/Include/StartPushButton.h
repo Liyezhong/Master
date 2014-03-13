7a79f569ca0bef8915f93d556eb8b09f30a29095
abe yang <abe.yang@leica-microsystems.com>
2014-03-13 13:15:25 +0800

    Customized Start and Pause pushButton.

---
+++ HimalayaMain/Gui/Components/Dashboard/Include/StartPushButton.h
@@ -0,0 +1,51 @@
+/****************************************************************************/
+/*! \file StartPushButton.h
+ *
+ *  \brief Header file for class CStartPushButton.
+ *
+ *   $Version: $ 0.1
+ *   $Date:    $ 2014-03-12
+ *   $Author:  $ A.Yang
+ *
+ *  \b Company:
+ *
+ *       Leica Microsystems Ltd. Shanghai.
+ *
+ *  (C) Copyright 2014 by Leica Microsystems Shanghai. All rights reserved.
+ *  This is unpublished proprietary source code of Leica. The copyright notice
+ *  does not evidence any actual or intended publication.
+ *
+ */
+/****************************************************************************/
+
+#ifndef DASHBOARD_STARTPUSHBUTTON_H
+#define DASHBOARD_STARTPUSHBUTTON_H
+
+#include <QPushButton>
+
+namespace Dashboard {
+  
+
+/****************************************************************************/
+/**
+ * \brief This is a customized Start/Abort button for Himalaya Project.
+ */
+/****************************************************************************/
+class CStartPushButton : public QPushButton
+{
+    Q_OBJECT
+
+public:
+    explicit CStartPushButton(QWidget *p_Parent = 0);
+    virtual ~ CStartPushButton();
+    void setText(const QString &text);
+    void IsStartStatus(bool);
+private:
+    void paintEvent(QPaintEvent *);
+    QString m_Text;
+    bool m_IsStartStatus;
+};
+
+} // end namespace Dashboard
+
+#endif // DASHBOARD_STARTPUSHBUTTON_H
