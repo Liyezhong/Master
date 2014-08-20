e7f75677763eee953d7da56469e6bb11176d736d
Sunny Qu <sunny.qu@leicabiosystems.com>
2014-08-20 18:03:12 +0800

    S&M: diable to display Chinese text

--- ServiceSW/Master/Components/Core/Source/Startup.cpp
+++ ServiceSW/Master/Components/Core/Source/Startup.cpp
@@ -668,9 +668,11 @@ void CStartup::ManufacturingGuiInit(bool bReInit)
         mp_ManaufacturingDiagnosticsHandler->ShowMessage(QApplication::translate("Core::CStartup", "System is initializing ...", 0, QApplication::UnicodeUTF8));
     }
 
+#if 0 // disabled by Sunny
     if (!bReInit) {
         mp_ServiceConnector->SetLanguage(PlatformService::CHINESE);
     }
+#endif
     qDebug()<<"CStartup::ManufacturingGuiInit finished";
 }
 
