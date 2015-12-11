34370841d6e0b18e15ff4622bf46fabdfee70a48
abe yang <abe.yang@leica-microsystems.com>
2015-12-11 11:55:23 +0800

    Improved for Hungarian Translation.

--- HimalayaMain/Gui/Components/Dashboard/Source/DashboardProgramStatusWidget.cpp
+++ HimalayaMain/Gui/Components/Dashboard/Source/DashboardProgramStatusWidget.cpp
@@ -76,7 +76,7 @@ void CDashboardProgramStatusWidget::ResizeHorizontalSection()
     mp_TableWidget->horizontalHeader()->resizeSection(1, 180);
     mp_TableWidget->horizontalHeader()->resizeSection(2, 100);
     mp_TableWidget->horizontalHeader()->resizeSection(3, 75);
-    mp_TableWidget->horizontalHeader()->resizeSection(4, 55);
+    mp_TableWidget->horizontalHeader()->resizeSection(4, 70);
     mp_TableWidget->horizontalHeader()->resizeSection(5, 45);
 }
 
