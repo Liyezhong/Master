071f91822c1332c8e5884656f8efadc1fef4aab4
Songtao Yu <songtao.yu@biosystems.com>
2016-05-23 15:50:12 +0800

    Change time format for 12H/24H format

--- HimalayaMain/Master/Components/Scheduler/Source/SchedulerMainThreadController.cpp
+++ HimalayaMain/Master/Components/Scheduler/Source/SchedulerMainThreadController.cpp
@@ -1034,19 +1034,52 @@ void SchedulerMainThreadController::CheckResuemFromPause(SchedulerStateMachine_t
     endTime = Global::AdjustedTime::Instance().GetCurrentDateTime().addSecs(-diff);
     if (offset > 1000*60) //If offset is less than one minute, we will NOT notify end user
     {
+        Global::TimeFormat timeFormat = mp_DataManager->GetUserSettings()->GetTimeFormat();
         switch(mp_DataManager->GetUserSettings()->GetDateFormat())
         {
         case Global::DATE_INTERNATIONAL:
-            RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("dd.MM.yyyy hh:mm"));
+            if (timeFormat == Global::TIME_12)
+            {
+                QString str = endTime.date().toString("dd.MM.yyyy") + " " + endTime.time().toString("hh:mm ap");
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<str);
+            }
+            else
+            {
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("dd.MM.yyyy hh:mm"));
+            }
             break;
         case Global::DATE_ISO:
-            RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("yyyy-MM-dd hh:mm"));
+            if (timeFormat == Global::TIME_12)
+            {
+                QString str = endTime.date().toString("yyyy-MM-dd") + " " + endTime.time().toString("hh:mm ap");
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<str);
+            }
+            else
+            {
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("yyyy-MM-dd hh:mm"));
+            }
             break;
         case Global::DATE_US:
-            RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("MM/dd/yyyy hh:mm"));
+            if (timeFormat == Global::TIME_12)
+            {
+                QString str = endTime.date().toString("MM/dd/yyyy") + " " + endTime.time().toString("hh:mm ap");
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<str);
+            }
+            else
+            {
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("MM/dd/yyyy hh:mm"));
+            }
             break;
         default:
-            RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("yyyy-MM-dd hh:mm"));
+            if (timeFormat == Global::TIME_12)
+            {
+                QString str = endTime.date().toString("yyyy-MM-dd") + " " + endTime.time().toString("hh:mm ap");
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<str);
+            }
+            else
+            {
+                RaiseEvent(EVENT_SCHEDULER_PAUSE_ENDTIME_UPDATE, QStringList()<<endTime.toString("yyyy-MM-dd hh:mm"));
+            }
             break;
         }
     }
